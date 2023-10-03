9ebaa793a423f2fe7b94d38d1b052cb726b479db
Seweryn Kras <seweryn.kras@golem.network>
2023-10-03 14:40:42 +0200

    chore: prepare react package for publishing

---
+++ packages/react/src/index.ts
@@ -0,0 +1,8 @@
+export { YagnaProvider } from "./provider";
+export { useYagna } from "./useYagna";
+export { useExecutor, TaskExecutor, type ExecutorOptions } from "./useExecutor";
+export { useTask, type Worker } from "./useTask";
+export { useInvoices, InvoiceStatus } from "./useInvoices";
+export { useDebitNotes } from "./useDebitNotes";
+export { useHandleInvoice } from "./useHandleInvoice";
+export { useHandleDebitNote } from "./useHandleDebitNote";
--- packages/react/src/useConfig.ts
+++ packages/react/src/useConfig.ts
@@ -1,12 +1,12 @@
 import { useContext } from "react";
-import { yagnaContext } from "./provider";
+import { yagnaContext, YagnaContext } from "./provider";
 
 /**
  * Returns the configuration object from the nearest `YagnaProvider` component in the component tree.
  * @throws {Error} If used outside the `YagnaProvider` component.
  * @internal
  */
-export function useConfig() {
+export function useConfig(): YagnaContext {
   const config = useContext(yagnaContext);
   if (!config) {
     throw new Error("`useConfig` must be used within a `YagnaProvider`");
